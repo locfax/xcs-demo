e789ef7200cc8996b3606c18d3a9b983307e3274
locphp <hellophp@qq.com>
2018-06-12 09:56:16 +0800

    -

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 define('APPKEY', 'web'); //应用关键字 用于识别配置、加载controller 权限验证文件等
 define('APPNAME', '社区'); //应用名称
 
-define('PSROOT', dirname(__DIR__)); //根目录 protected source所在的目录
+define('PSROOT', __DIR__); //根目录 protected source所在的目录
 define('APPDSN', 'portal'); //默认使用的数据库DSN ID
 define('DEBUG', true); //debug模式
 define('AUTH', false); //是否检验权限 false 不检测权限 / general 普通 有设置权限就检测  没设置就忽略/ strict 严格 必须设置权限
